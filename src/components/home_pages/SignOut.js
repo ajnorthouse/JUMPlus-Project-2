6f676288c761694584950d12f14ed1132250df1c
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-04-19 11:31:40 -0400

    Implement SignOut

--- src/components/home_pages/SignOut.js
+++ src/components/home_pages/SignOut.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import { Redirect } from "react-router";
 
-export default function SignOut() {
+export default function SignOut(props) {
+
+  //Sets login to [null] on the redirect
+	useEffect(() => {
+    //hook equivalent of "componentWillUnmount "
+    return () => {
+      if (props.login.length < 2) {
+        props.setLogin([null]);
+      }
+    }
+  }, [props]);
+
 	return (
 		<div className="sign-out">
-			<p>Sign Out Page</p>
+			<h1>Sign Out Page</h1>
 			<Redirect to="/welcome"/>
 		</div>
 	);
