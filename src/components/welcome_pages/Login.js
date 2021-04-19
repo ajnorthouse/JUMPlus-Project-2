0796d6c977f2a0a653e564bf50fb0d8f4a4af4a7
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-04-19 10:11:33 -0400

    Missing import

--- src/components/welcome_pages/Login.js
+++ src/components/welcome_pages/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Redirect } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function Login(props) {
     //State constants
