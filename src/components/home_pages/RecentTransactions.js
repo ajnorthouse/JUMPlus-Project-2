b6f94d667c5cb7e0faf54e905c4630b0795b40c5
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-04-19 11:31:30 -0400

    Implement Basic RecentTransactions

--- src/components/home_pages/RecentTransactions.js
+++ src/components/home_pages/RecentTransactions.js
@@ -1,7 +1,34 @@
-export default function RecentTransactions() {
+import { Link } from "react-router-dom";
+
+export default function RecentTransactions(props) {
+
+
+  // TODO: Check for logged-in user
+
+
 	return (
 		<div className="recent_transactions">
-			<p>Recent Transactions Page</p>
+			<h1>Recent Transactions Page</h1>
+
+      <ul>
+        <li>
+          {props.login.log[0]}
+        </li>
+        <li>
+          {props.login.log[1]}
+        </li>
+        <li>
+          {props.login.log[2]}
+        </li>
+        <li>
+          {props.login.log[3]}
+        </li>
+        <li>
+          {props.login.log[4]}
+        </li>
+      </ul>
+			
+      <Link to="/home/">Home</Link>
 		</div>
 	);
 }
